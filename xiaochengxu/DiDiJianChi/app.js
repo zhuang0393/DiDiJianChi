9cb2de0f6f8b852b4bef23b75397eb6e90c8ced6
hzh0393 <zhuang0393@163.com>
2017-02-19 21:39:58 +0800

    start with leantodo

--- xiaochengxu/DiDiJianChi/app.js
+++ xiaochengxu/DiDiJianChi/app.js
@@ -1,59 +1,10 @@
-//app.js
-App({
-  globalData: {
-    userInfo: null,
-    //应用设置
-    appSetting: {
-      goal: 'default',
-      time: '30',
-      bet: '50'
-    }
-  },
-  constant: {
-    SETTING: 'INSIST_SETTING',
-    CACHE: 'INSIST_CACHE'
-  },
-  debug: false, //程序调试
-
-  onLaunch: function () {
-    console.log('app Launching ...');
-    var _this = this;
-    //获取应用设置
-    var settingData = wx.getStorageSync( _this.constant.SETTING );
-    if( settingData ) {
-      this.globalData.appSetting = settingData;
-    }
-  },
-  getUserInfo: function( cb ) {
-    var that = this
-    if( this.globalData.userInfo ) {
-      typeof cb == "function" && cb( this.globalData.userInfo )
-    } else {
-      //调用登录接口
-      wx.login( {
-        success: function() {
-          wx.getUserInfo( {
-            success: function( res ) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb( that.globalData.userInfo )
-            }
-          })
-        }
-      })
-    }
-  },
-  //更新应用设置
-  updateupdateAppSetting: function( data ) {
-    var _this = this;
-    try {
-      wx.setStorageSync( _this.constant.SETTING, data );
-    } catch( e ) {
-      return false;
-    }
-    return true;
-  },
-  //获取缓存
-  getCache: function() {
-    return wx.getStorageSync( _this.constant.CACHE );
-  },
+const AV = require('./utils/av-weapp-min');
+var APP_ID = 'guyJXb7uR3KKA7kD9Ph7ovIt-gzGzoHsz';
+var APP_KEY = '46PfO6jbdLccm9KnlpWFcK1N';
+AV.init({ 
+   appId: APP_ID, 
+   appKey: APP_KEY, 
 });
+
+//app.js
+App({});
